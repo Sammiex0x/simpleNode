4a2c996f1897614902c352cd337221f731918717
Sammiex0x <Sammiex0x@hotmail.com>
2017-03-27 23:38:21 -0700

    updated need help with css

--- sql_ejs_example.js
+++ sql_ejs_example.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+ <link rel="stylesheet" type="text/css" href="example.css">
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
